ede536d87fe90bae893f41ae1235f3e90f3caea9
Sam <samellis1403@gmail.com>
2025-05-20 20:22:54 -0400

    changes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+    return (
+
+        <div>
+            {children}
+        </div>
+
+
+    );
+}
+
+export default RootLayout;
