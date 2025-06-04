620d51e5dc4bed790f44506d5a86e3f66b63405b
Sam <samellis1403@gmail.com>
2025-06-04 18:10:46 -0400

    updated

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { vapi } from '@/lib/vapi.sdk';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action'
 
 
 
@@ -71,10 +72,11 @@ const Agent = ({ userName, userId, type, interviewId, questions  }: AgentProps) 
         console.log('Generate feedback here.');
 
 
-        const { success, id }= {
-            success: true, 
-            id: 'feedback-id'
-        }
+        const { success, feedbackId }= await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages
+        })
 
         if(success && id ) {
             router.push(`/interview/${interviewId}/feeback`);
